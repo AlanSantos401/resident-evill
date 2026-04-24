e532bb28e875a37a8bcfa96ea7861222e4a33567
Alan <alandsantos54@gmail.com>
2026-04-23 21:35:34 -0300

    responsividade para celular

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -63,26 +63,26 @@ export default function Home() {
       />
 
       <section>
-        <div className='flex flex-col justify-center pt-14 p-8'>
-          <img className='w-xl' src={resident} alt="nome-resident-evill" />
+        <div className='flex flex-col justify-center px-5 lg:pt-14 lg:p-8'>
+          <img className='w-2xs lg:w-xl' src={resident} alt="nome-resident-evill" />
 
-          <h2 className='text-3xl mb-4 text-mauve-300 leading-relaxed tracking-wider'>
+          <h2 className='text-xl lg:text-3xl mb-3 lg:mb-4 text-mauve-300 leading-relaxed tracking-wider'>
             ARQUIVOS OFICIAIS DA SAGA
           </h2>
 
-          <p className='text-xl text-mauve-300'>
+          <p className='text-[1rem] lg:text-xl text-mauve-300'>
             Explore a cronologia completa, personagens lendários, locais <br />
             icônicos e os segredos mais sombrios da Umbrella Corporation.
           </p>
 
-          <div className="flex gap-4 mt-8">
+          <div className="flex gap-4 mt-4 lg:mt-8">
 
             <button
               onClick={() => {
                 playClick();
                 navigate("/organizacao");
               }}
-              className="group relative flex items-center gap-2 px-6 py-4 cursor-pointer 
+              className="group relative flex items-center gap-2 px-4 py-2 lg:px-6 lg:py-4 cursor-pointer 
               text-white uppercase overflow-hidden
               border border-red-600 bg-red-600/20
               transition hover:bg-red-600/30 hover:shadow-[0_0_12px_#ff1a1a]"
@@ -95,12 +95,12 @@ export default function Home() {
                 [clip-path:polygon(10px_0%,calc(100%-10px)_0%,100%_10px,100%_calc(100%-10px),calc(100%-10px)_100%,10px_100%,0%_calc(100%-10px),0%_10px)]" />
 
               <img
-                className="w-8 z-10 transition-transform duration-500 group-hover:rotate-[360deg] group-hover:scale-110"
+                className="w-6 lg:w-8 z-10 transition-transform duration-500 group-hover:rotate-[360deg] group-hover:scale-110"
                 src={umbrella}
                 alt="icon-umbrella"
               />
 
-              <span className="text-xl z-10 tracking-wide">
+              <span className="text-sm lg:text-xl z-10 tracking-wide">
                 VER ORGANIZAÇÃO
               </span>
             </button>
@@ -110,7 +110,7 @@ export default function Home() {
                 playClick();
                 navigate("/linha-do-tempo");
               }}
-              className="relative flex items-center gap-2 px-8 py-4 cursor-pointer 
+              className="relative flex items-center gap-2 px-6 lg:px-8 lg:py-4 cursor-pointer 
               text-white uppercase overflow-hidden
               border border-gray-600 bg-gray-700/20
               transition hover:bg-gray-700/30 hover:shadow-[0_0_10px_rgba(255,255,255,0.2)]"
@@ -119,7 +119,7 @@ export default function Home() {
               <div className="absolute inset-0 border border-gray-600 pointer-events-none
                 [clip-path:polygon(10px_0%,calc(100%-10px)_0%,100%_10px,100%_calc(100%-10px),calc(100%-10px)_100%,10px_100%,0%_calc(100%-10px),0%_10px)]" />
 
-              <span className="text-xl z-10">
+              <span className="text-sm lg:text-xl z-10">
                 VER LINHA DO TEMPO
               </span>
             </button>
